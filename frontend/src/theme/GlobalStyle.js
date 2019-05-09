a47b1a4d876dc233813c55497faef663b15bcfa3
Joan Maria Talarn <joanmaria.talarn@rakuten.com>
2019-05-09 09:47:43 +0200

    First commit

---
+++ frontend/src/theme/GlobalStyle.js
@@ -0,0 +1,53 @@
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+export default createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+    
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 2rem;
+    line-height: 2;
+    background: ${props => props.theme.black};
+    color: ${props => props.theme.offWhite};
+  }
+  
+  .App {
+  text-align: center;
+}
+
+.App-logo {
+  animation: App-logo-spin infinite 20s linear;
+  height: 40vmin;
+  pointer-events: none;
+}
+
+.App-header {
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+}
+
+.App-link {
+  color: #61dafb;
+}
+
+@keyframes App-logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+  `;
---
+++ frontend/src/theme/Theme.js
@@ -0,0 +1,13 @@
+const Theme = {
+  red: "#FF0000",
+  yellow: "yellow",
+  darkBlack: "#000000",
+  orange: "orange",
+  darkOrange: "#d48900",
+  black: "#393939",
+  grey: "#3A3A3A",
+  lightgrey: "#E1E1E1",
+  offWhite: "#EDEDED",
+};
+
+export default Theme;
