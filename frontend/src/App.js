ee9837b51bd2fa238d74061278810b3a8954744d
Joan Maria Talarn <joanmaria.talarn@rakuten.com>
2019-05-09 10:22:33 +0200

    Layou

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,18 @@ import { ThemeProvider } from "styled-components";
 import theme from "./theme/Theme";
 import GlobalStyle from "./theme/GlobalStyle";
 import Header from "./components/Header";
+import SearchProduct from "./components/SearchProduct";
+import TicketDashboard from "./components/TicketDashboard";
+import Layout from "./components/Layout";
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         <Header />
+        <Layout>
+          <SearchProduct />
+          <TicketDashboard />
+        </Layout>
         <GlobalStyle />
       </div>
     </ThemeProvider>
---
+++ frontend/src/components/Layout.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export default styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 0.5rem;
+  padding: 1rem;
+  border: 1px dotted red;
+`;
