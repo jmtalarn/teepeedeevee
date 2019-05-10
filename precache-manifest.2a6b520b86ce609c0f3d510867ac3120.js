033b099f89d0f4604f461c3b24e585c02ccf27b1
Joan Maria Talarn <joanmaria.talarn@rakuten.com>
2019-05-10 10:09:16 +0200

    Updates

---
+++ precache-manifest.2a6b520b86ce609c0f3d510867ac3120.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703e2f9ad608cfe0734a0deff0d0a87e",
+    "url": "/teepeedeevee/index.html"
+  },
+  {
+    "revision": "235331eec3b8a2326eab",
+    "url": "/teepeedeevee/static/js/2.73747369.chunk.js"
+  },
+  {
+    "revision": "ded492016c87d2a16bbe",
+    "url": "/teepeedeevee/static/js/main.a0baa096.chunk.js"
+  },
+  {
+    "revision": "abb65c311ebd59a5f7eb",
+    "url": "/teepeedeevee/static/js/runtime~main.b7b3ed20.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/teepeedeevee/static/media/logo.5d5d9eef.svg"
+  }
+]);
