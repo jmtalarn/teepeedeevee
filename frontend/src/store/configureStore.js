0439474652f1edc7af2bf0bd0ff1fbe011493331
jmtalarn <jmtalarn@gmail.com>
2025-01-09 16:26:56 +0100

    Fix

--- frontend/src/store/configureStore.js
+++ frontend/src/store/configureStore.js
@@ -1,17 +1,20 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import rootReducer from "../reducers/rootReducer";
 import thunk from "redux-thunk";
 import { createLogger } from "redux-logger";
 import createCLILogger from "redux-cli-logger";
 
 const logger =
-  process.env.NODE_ENV === "test" ? createCLILogger({}) : createLogger();
+	process.env.NODE_ENV === "test" ? createCLILogger({}) : createLogger();
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore() {
-  return createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk, logger),
-  );
+	return createStore(
+		rootReducer,
+		composeEnhancer(applyMiddleware(thunk, logger))
+		// window.__REDUX_DEVTOOLS_EXTENSION__ &&
+		//   window.__REDUX_DEVTOOLS_EXTENSION__(),
+		// applyMiddleware(thunk, logger),
+	);
 }
